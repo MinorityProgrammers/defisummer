faede251e720e78d2879c6412265cdd5035671e9
Evan Uwao <uwaoevanz@gmail.com>
2021-05-29 15:47:01 +0300

    This is the dummy data

---
+++ src/Data/DummyData.js
@@ -0,0 +1,25 @@
+import Help1Img from '../assets/icons/Group 52.svg'
+import Help2Img from '../assets/icons/Group 66.svg'
+import Help3Img from '../assets/icons/Group 58.svg'
+import Help4Img from '../assets/icons/Group 83.svg'
+import Help5Img from '../assets/icons/Group 49.svg'
+
+export const data = {
+    prize: [
+        {id: '1', title: 'Learning Incentivization', description: 'Learning Incentivization: Develop an e-learning application that rewards individuals crypto for learning STEM concepts. The winners of this will be given a chance at Blockchain Engineering internships with MPA.'},
+        {id: '2', title: 'Blockchain Consulting Award (2)', description: 'Blockchaun Consulting: Case Study - Design a system that turns Minority Programmers Association into a DAO with its own utility tocken. Winner with be given an Internship as Blockchain Research Intern at Minority Programmers or Echelon Digitak Consulting.'},
+        {id: '3', title: '“Choose Your Own Adventure Governance” - PlutoPepe ($1000 in $SPLUTO)', description: 'Build a dAPP with a stunning UI that allows individuals to vote on the outcome of a storyline for having a certain amount of tockens in their wallet.'},
+        {id: '4', title: "Gaming Utility Token - PlutoPepe ($1000 in $PLUTO)", description: 'This goes to the team that can implement a smart contract game; ex; ConFlip, Predictions, Roulette, Dice, that pays out rewards in $PEPE token.'},
+        {id: '5', title: 'DefiSummer NFT Art Grand Prize', description: 'You are to design a one of a kind DeFiSummer NFT. Make it iconic. Think the DeFi Woodstock. Your non-fungible token will be forever minted in DeFI Hall of Fame.'},
+        {id: '6', title: 'Trading Automation Prize - Enzyme', description: 'Trading Automation: Develop an internface and bot that allows gedge funds on Enzyme finance to easily build trading strategies, fund allocation, etc. generates and a bot that uses the enzyme protocal to trade accordingly.'},
+        {id: '7', title: 'NFT Meme Marketplace Prize - PlutoPepe ($1000 in $PLUTO)', description: 'NFT Meme Marketplace - Develop an NFT marketplace on BSC/Matic that allows anyone to easily build a meme, use as nft, share on social media, bid, build and view and even respond with NFT comments.'},
+        {id: '8', title: 'Stellar Prize ', description: 'Build a dAPP that facilitates use case of global (cross-border) payment system on Stellar testnet/mainnet; Ex; developing a system for paying out hourly wages for certain job positions.'},
+    ],
+    help: [
+        {id: '1', image:Help1Img , title: 'Host a Workshop'},
+        {id: '2', image:Help2Img , title: 'Become a Judge'},
+        {id: '3', image:Help3Img , title: 'Become a Mentor'},
+        {id: '4', image:Help4Img , title: 'Do Outreach & Marketing'},
+        {id: '5', image:Help5Img , title: 'Contribute Ideas'},
+    ]
+}
