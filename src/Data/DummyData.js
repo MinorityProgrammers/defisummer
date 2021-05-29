dc6217e802d573126406118cb9158061d3ce16a4
Evan Uwao <uwaoevanz@gmail.com>
2021-05-29 20:03:47 +0300

    Updated the json data

--- src/Data/DummyData.js
+++ src/Data/DummyData.js
@@ -1,8 +1,22 @@
+//Help Icons
 import Help1Img from '../assets/icons/Group 52.svg'
 import Help2Img from '../assets/icons/Group 66.svg'
 import Help3Img from '../assets/icons/Group 58.svg'
 import Help4Img from '../assets/icons/Group 83.svg'
 import Help5Img from '../assets/icons/Group 49.svg'
+
+//Sponsors
+import Stellar from '../assets/images/Stellar_Development_Foundation.svg'
+import Mpa from '../assets/images/MPA-Exec.svg'
+import Mtt from '../assets/images/MTTLogo 3.svg'
+import Pluto from '../assets/images/pluto.svg'
+import Saint from '../assets/images/saintgits-white.svg'
+import Echelon from '../assets/images/image 63.svg'
+import Blockchain from '../assets/images/image 67.svg'
+
+//Socials
+import Twitter from '../assets/icons/Rectangle 272.svg'
+import Second from '../assets/icons/Rectangle 271.svg'
 
 export const data = {
     prize: [
@@ -21,5 +35,36 @@ export const data = {
         {id: '3', image:Help3Img , title: 'Become a Mentor'},
         {id: '4', image:Help4Img , title: 'Do Outreach & Marketing'},
         {id: '5', image:Help5Img , title: 'Contribute Ideas'},
+    ],
+    faq: [
+        {id: '1', title: 'What is DeFi? ', answer: 'Decentralized finance is an experimental form of finance that does not rely on central financial intermediaries such as brokerages, exchanges, or banks to offer traditional financial instruments, and instead utilizes smart contracts on blockchains, the most common being Ethereum.'},
+        {id: '2', title: 'How long is DefiSummerHacks?', answer: 'All Summer. Broken up into 4 parts. The first month a series of educational events (all June), the next 1.5 months (July-midAugust) is the hackathon build, the next week a summit of leading speakers (August 16- 22), and the final week a celebration (August 23-31st)!'},
+        {id: '3', title: 'Who can participate?', answer: 'Anyone is welcome to join! Defi is for everybody.'},
+        {id: '4', title: 'How can I get prepared before June?', answer: 'Start by learning blockchain, smart contracts, and code reviewing the leading blockchain technologies.'},
+    ],
+    judges:[
+        {id:'1', name: 'Shadman Hossain', position: 'Blockchain Engineer', image: 'https://picsum.photos/200'},
+        {id:'2', name: 'Roshaan Khan', position: 'Product/ Strategy @Block-One', image: 'https://picsum.photos/200'},
+        {id:'3', name: 'Mona El Isa', position: 'Founder of Enzyme Finance', image: 'https://picsum.photos/200'},
+        {id:'4', name: 'Robin Roy Thomas', position: 'Content Writer, Ethereum Push, Notification Services', image: 'https://picsum.photos/200'},
+        {id:'5', name: 'Kirill Kazakov', position: 'Senior DevOps Engineer', image: 'https://picsum.photos/200'},
+        {id:'6', name: 'Alexander Makeev', position: 'Software Engineer @ Arcadia', image: 'https://picsum.photos/200'},
+        {id:'7', name: 'Taras Baranyuk', position: 'Senior Data Scientist @Bideose', image: 'https://picsum.photos/200'},
+        {id:'8', name: 'David Medvedik', position: 'Principal Software Engineer', image: 'https://picsum.photos/200'},
+        {id:'9', name: 'Aliaksandr Shulyak', position: 'Senior SRE', image: 'https://picsum.photos/200'},
+        {id:'10', name: 'Aleksandr Zakharov', position: 'Software Engineer at Recall Masters, Inc', image: 'https://picsum.photos/200'},
+    ],
+    sponsors:[
+        {id: '1', image: Stellar},
+        {id: '2', image: Mpa},
+        {id: '3', image: Mtt},
+        {id: '4', image: Pluto},
+        {id: '5', image: Saint},
+        {id: '6', image: Echelon},
+        {id: '7', image: Blockchain},
+    ],
+    socials:[
+        {id: '1', image: Second},
+        {id: '2', image: Twitter},
     ]
 }
